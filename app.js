4627bda32163284e6c6384f1154aba587bfadb5c
HyeonGi2 <tmskc56@gmail.com>
2022-05-24 12:51:49 +0900

    cors 허용

---
+++ app.js
@@ -0,0 +1,58 @@
+// app.js
+import express from "express";
+import mysql from "mysql2/promise";
+import cors from "cors";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "a9",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: "http://cdpn.io",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+const port = 3000;
+
+app.get("/todos/:id", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY RAND() limit 1;
+    `,
+    [id]
+  );
+
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  res.json(rows[0]);
+});
+
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.listen(port);
